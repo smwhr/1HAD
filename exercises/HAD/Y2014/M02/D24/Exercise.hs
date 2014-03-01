ef7207995374a08317b4d75be00989ed3d638bed
Ju/TroisYaourts <julien@troisyaourts.com>
2014-03-01 17:56:33 +0100

    solved 2014 02 24

--- exercises/HAD/Y2014/M02/D24/Exercise.hs
+++ exercises/HAD/Y2014/M02/D24/Exercise.hs
@@ -1,5 +1,7 @@
 module HAD.Y2014.M02.D24.Exercise where
 
+import Data.List
+
 -- | Filter a list, keeping an element only if it is equal to the next one.
 --
 -- Examples:
@@ -10,5 +12,5 @@ module HAD.Y2014.M02.D24.Exercise where
 -- >>> filterByPair [1, 2, 2, 2, 3, 3, 4]
 -- [2,2,3]
 
---filterByPair :: Find the most generic signature
-filterByPair = undefined
+filterByPair :: Eq a => [a] -> [a]
+filterByPair = (>>= init).group
